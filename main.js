d7c84c13da63a1640101fecf470959f8d8928f3b
Yuka Langbuana <langbuana.yuka@hotmail.com>
2019-04-29 02:56:23 -0700

    rolling out new version for new model

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var database_url = "http://syahbandar-osiris.herokuapp.com/api/predict";
+    var database_url = "http://localhost:5000/api/predict";
 
     String.prototype.format = function () {
         var i = 0, args = arguments;
@@ -23,9 +23,9 @@ $(document).ready(function(){
             data: "{\"text\":{}}".format(JSON.stringify($('#news-input').val().replace(/\n/g, ""))),
             success: function (data) {
                 if (data['real_status'] == true){
-                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><img class=\"img-fluid img-thumbnail\" src=\"benar.gif\"><br><br><p>Kelihatannya sih ini berita beneran. Tapi coba cek lagi biar tambah yakin.</p>".format(data['message']))
+                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><br><p><b><i>Confidence Level : </i>{}%</b></p><img class=\"img-fluid img-thumbnail\" src=\"benar.gif\"><br><br><p>Kelihatannya sih ini berita beneran. Tapi coba cek lagi biar tambah yakin.</p>".format(data['message'], (data['real_confidence']*100).toFixed(1)))
                 } else {
-                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><img class=\"img-fluid img-thumbnail\" src=\"konfirmasi.gif\"><br><br><p>Berita darimana nih? Nampaknya berita ini kurang faktual. Coba cek lagi sumbernya, jangan sampai kamu ikut nyebarin hoax ya!</p>".format(data['message']))
+                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><br><p><b><i>Confidence Level : </i>{}%</b></p><img class=\"img-fluid img-thumbnail\" src=\"konfirmasi.gif\"><br><br><p>Berita darimana nih? Nampaknya berita ini kurang faktual. Coba cek lagi sumbernya, jangan sampai kamu ikut nyebarin hoax ya!</p>".format(data['message'], (data['opinion_confidence']*100).toFixed(1)))
                 }
         }});
 
