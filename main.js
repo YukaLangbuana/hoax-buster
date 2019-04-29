1bafe073cd5aab60f3b7c4faa41e5b4fe4bc037e
Yuka Langbuana <langbuana.yuka@hotmail.com>
2019-04-29 03:13:32 -0700

    moving confidence level

--- main.js
+++ main.js
@@ -23,9 +23,9 @@ $(document).ready(function(){
             data: "{\"text\":{}}".format(JSON.stringify($('#news-input').val().replace(/\n/g, ""))),
             success: function (data) {
                 if (data['real_status'] == true){
-                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><br><p><b><i>Confidence Level : </i>{}%</b></p><img class=\"img-fluid img-thumbnail\" src=\"benar.gif\"><br><br><p>Kelihatannya sih ini berita beneran. Tapi coba cek lagi biar tambah yakin.</p>".format(data['message'], (data['real_confidence']*100).toFixed(1)))
+                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><img class=\"img-fluid img-thumbnail\" src=\"benar.gif\"><br><br><p><b><i>Confidence Level : </i>{}%</b></p><p>Kelihatannya sih ini berita beneran. Tapi coba cek lagi biar tambah yakin.</p>".format(data['message'], (data['real_confidence']*100).toFixed(1)))
                 } else {
-                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><br><p><b><i>Confidence Level : </i>{}%</b></p><img class=\"img-fluid img-thumbnail\" src=\"konfirmasi.gif\"><br><br><p>Berita darimana nih? Nampaknya berita ini kurang faktual. Coba cek lagi sumbernya, jangan sampai kamu ikut nyebarin hoax ya!</p>".format(data['message'], (data['opinion_confidence']*100).toFixed(1)))
+                    $('.modal-body').html("<h2 style=\"text-align:center\">{}</h2><img class=\"img-fluid img-thumbnail\" src=\"konfirmasi.gif\"><br><br><p><b><i>Confidence Level : </i>{}%</b></p><p>Berita darimana nih? Nampaknya berita ini kurang faktual. Coba cek lagi sumbernya, jangan sampai kamu ikut nyebarin hoax ya!</p>".format(data['message'], (data['opinion_confidence']*100).toFixed(1)))
                 }
         }});
 
